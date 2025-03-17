96bdfd47b3d01ff0a0428508295a1a99338a9af6
Admin <ekaterina28041998@gmail.com>
2025-03-17 22:02:35 +0300

    массивы 1. ветка дз. доработка 1

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
         int[] numbers1 = {1, 2, 3};
         System.out.println(Arrays.toString(numbers1));
-
+        for (int i = 0; i < numbers1.length; i++) {
+            if (i < numbers1.length)
+                System.out.println(numbers1[i] + " ");
+        }
         for (int i = numbers1.length - 1; i >= 0; i--) {
-
             if (numbers1[i] % 2 != 0) {
                 numbers1[i] += 1;
             }
@@ -17,28 +18,44 @@ public class Main {
         double[] numbers2 = {1.57, 7.654, 9.986};
         System.out.println(Arrays.toString(numbers2));
 
-        for (int j = numbers2.length - 1; j >= 0; j--)
-
-            if (numbers2[j] % 2 != 0) {
-                numbers2[j] += 1;
-            }
-        {
-            System.out.println(Arrays.toString(numbers2));
+        for (int i = 0; i < numbers2.length; i++) {
+            if (i < numbers2.length)
+                System.out.println(numbers2[i] + " ");
         }
+        for (int i = numbers2.length - 1; i >= 0; i--) {
+            if (numbers2[i] % 2 != 0)
+                numbers2[i] += 1;
+        }
+        System.out.println(Arrays.toString(numbers2));
+
+        System.out.println();
+        int[] numbers3 = {5, 6, 10};
+        System.out.println(Arrays.toString(numbers3));
         {
-            System.out.println();
-            int[] numbers3 = {5, 6, 10};
-            System.out.println(Arrays.toString(numbers3));
-            for (int k = numbers3.length - 1; k >= 0; k--) {
-                System.out.print(numbers3[k] + ", ");
-                if (numbers3[k] % 2 != 0) {
-                    numbers3[k] += 1;
+
+            for (int i = numbers3.length - 1; i >= 0; i--) {
+                System.out.print(numbers3[i] + " ");
+
+                if (numbers3[i] % 2 != 0)
+                    numbers3[i] += 1;
+                {
+
+
+                        if (i < numbers3.length - 1)
+                            System.out.println(numbers3[i] + ", ");
+                        else {
+                            System.out.println(numbers3[i] + " ");
+
+                        }
+
+                    }
                 }
             }
-
-
         }
     }
-    }
+
+
+
+
 
 
